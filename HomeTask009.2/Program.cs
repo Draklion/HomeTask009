006799243620f3eb4abc556814887745d715009c
«Dmitry_Machnev» <draklion56@gmail.com>
2022-09-07 22:36:48 +0500

    HomeTask009.2 completed.

--- HomeTask009.2/Program.cs
+++ HomeTask009.2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void CWL(string msg)
+{
+  Console.WriteLine(msg);
+}
+void CW(string msg)
+{
+  Console.Write(msg);
+}
+string CR()
+{
+  return Console.ReadLine();
+}
+try
+{
+  void recursion(int N, int sum = 0)
+  {
+    if (N % 10 == 0) CW($"{sum}");
+    if (N % 10 != 0)
+    {
+      sum = sum + N % 10;
+      recursion(N / 10, sum);
+    }
+  }
+
+  CWL("Задание 2. Программа, которая будет принимать на вход число и возвращать сумму его цифр.");
+  CWL("Введите число: ");
+  int number = Int32.Parse(CR());
+  CW($"Сумма цифр числа {number} равна: ");
+  recursion(number);
+}
+catch (InvalidCastException e) { }
