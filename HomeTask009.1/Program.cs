4a3d074b9945c0b01bbd1abd8e6507af0fc0ac3b
«Dmitry_Machnev» <draklion56@gmail.com>
2022-09-07 21:58:09 +0500

    HomeTask009.1 completed.

--- HomeTask009.1/Program.cs
+++ HomeTask009.1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void CWL(string msg)
+{
+  Console.WriteLine(msg);
+}
+void CW(string msg)
+{
+  Console.Write(msg);
+}
+string CR()
+{
+  return Console.ReadLine();
+}
+try
+{
+  void recursion(int N, int i = 1)
+  {
+    if (i <= N)
+    {
+      CW($"{i} ");
+      recursion(N, i + 1);
+    }
+  }
+
+  CWL("Задание 1. Программа, которая выведет все натуральные числа в промежутке от 1 до N.");
+  CWL("Введите число: ");
+  int number = Int32.Parse(CR());
+  CW($"Натуральные числа в промежутке от 1 до {number}: ");
+  recursion(number);
+}
+catch (InvalidCastException e) { }
